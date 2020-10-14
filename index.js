251cab41513f7f0beb39029e5669d12bf4e06d5a
raedmiranda <lalo_sagitario.95@hotmail.es>
2020-10-14 01:51:29 -0500

    updated

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var getFormData = require('js-getformdata')
+const getFormData = require('js-getformdata')
 const fs = require('fs')
+const { JSDOM } = require('jsdom')
+
+
 let testHTML = fs.readFileSync('index.html').toString();
-
-const { JSDOM } = require('jsdom');
 const jsdom = new JSDOM(testHTML);
-
 const { window } = jsdom;
-const { document } = window;
 const $ = global.jQuery = require('jquery')(window);
 
 
-let form = $("form");
-let odata = getFormData(form);
+let form = $("form"),
+odata = getFormData(form);
 console.log(odata)
